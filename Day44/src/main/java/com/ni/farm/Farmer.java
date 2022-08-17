cba1e8c7d3a95d31cd2ca172bca19ac049f2b5dd
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2022-08-17 17:55:03 +0530

    Controller, PostMapping, Forms-add-remove, Redirect

---
+++ Day44/src/main/java/com/ni/farm/Farmer.java
@@ -0,0 +1,16 @@
+package com.ni.farm;
+
+public class Farmer {
+    public String name;
+    public int age;
+    public float yearsOfExperience;
+
+    public Farmer() {
+    }
+
+    public Farmer(String name, int age, float yearsOfExperience) {
+        this.name = name;
+        this.age = age;
+        this.yearsOfExperience = yearsOfExperience;
+    }    
+}
